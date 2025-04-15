889ab43b92b7e712b9272dbc139189e584c25e6b
Kenz <ekeneokoye93@gmail.com>
2025-04-15 22:38:13 +0100

    color issue resolved

--- src/components/Callback.jsx
+++ src/components/Callback.jsx
@@ -4,7 +4,7 @@ import Loading from "./Loading";
 const Callback = () => {
   const [callback, setCallback] = useState(false);
 
-  return <>{!callback ? <Loading /> : <div>Unable to Authenticate</div>}</>;
+  return <Loading />;
 };
 
 export default Callback;
