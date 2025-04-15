41d21af6c14e4633e15cfeece108f33730b03e7d
Kenz <ekeneokoye93@gmail.com>
2025-04-15 22:51:13 +0100

    callback api

--- src/components/Callback.jsx
+++ src/components/Callback.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Callback = () => {
   const [callback, setCallback] = useState(false);
+  const {token,userId} = useParams()
+
+  const createUser = async()=>{
+    try {
+      const res = await axios.get(`${import.meta.env.VITE_BASE_URL}api/v1/studentInfo/${userId}`)
+      console.log(res)
+    } catch (error) {
+      toast.error(error?.response?.data?.message)
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    createUser()
+  },[])
 
   return <Loading />;
 };
