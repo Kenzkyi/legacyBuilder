a81be4c67b8915d5c89bba58c58c6a7312c08ebe
Benjamin <benjaminufuomajacob9@gmail.com>
2025-04-16 07:54:12 +0100

    done, but was not able to do validation for profile

--- src/pages/jacob/PastQuestion.jsx
+++ src/pages/jacob/PastQuestion.jsx
@@ -41,6 +41,18 @@ const PastQuestion = () => {
     "2016",
     "2015",
     "2014",
+    "2013",
+    "2012",
+    "2011",
+    "2010",
+    "2009",
+    "2008",
+    "2007",
+    "2006",
+    "2005",
+    "2004",
+    "2003",
+    "2002",
   ];
 
   const getPastQuestionForYearSubject = async (year, subject) => {
